a552942215496a463daf8526a8a1477005aa446b
kartheek nagineni <nagineni.kartheek@gmail.com>
2022-03-14 21:06:29 +0530

    Frequency of number

---
+++ Programs/Frequency.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int num,digit,t,count=0;
+  printf("Enter a number:");
+  scanf("%d",&num);
+  printf("Enter the digit to be counted:");
+  scanf("%d",&digit);
+  while(num!=0)
+  {
+    t=num%10;
+    if(t==digit)
+    count++;
+    num=num/10;
+  }
+  printf("The digit %d presents %d times",digit,count);
+  }
