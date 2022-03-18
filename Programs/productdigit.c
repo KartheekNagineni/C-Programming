92614dc38a09301e5ca298ecdcebb0c3e0564bff
kartheek nagineni <nagineni.kartheek@gmail.com>
2022-03-18 22:30:44 +0530

    Product Of Given Digit

---
+++ Programs/productdigit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int n,prod=1,rem;
+  printf("Enter a number:");
+  scanf("%d",&n);
+
+  while(n!=0)
+  {
+    rem=n%10;
+    prod*=rem;
+    n=n/10;
+  }
+  printf("Product of digits in a number=%d",prod);
+}
