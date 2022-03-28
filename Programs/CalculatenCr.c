745c4ced8d2af9a5de7c365154b63e0156bb747c
kartheek nagineni <nagineni.kartheek@gmail.com>
2022-03-28 19:08:23 +0530

    To Calculate nCr Value

---
+++ Programs/CalculatenCr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int factorial(int Number)
+{
+  if (Number==0||Number==1)
+    return 1;
+  else
+    return Number*factorial (Number-1);
+}
+int main()
+{
+    int n,r,ncrResult;
+    printf("Enter the Number:");
+    scanf("%d",&n);
+    printf("Enter the r Value:");
+    scanf("%d",&r);
+    ncrResult=factorial(n)/(factorial(r)*factorial(n-r));
+    printf("The Result of nCr is:%d\n",ncrResult);
+    return 0;
+}
