682c535125b564c85175d2759c99b0b8dca28d02
Kartheek Nagineni <79053787+naginenik@users.noreply.github.com>
2022-05-05 18:42:52 +0530

    Perimeter of a square

---
+++ Programs/perimeterofsquare.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float Sqrside,Sqrperimeter;
+    printf("Enter the Square Side:");
+    scanf("%f",&Sqrside);
+    Sqrperimeter=4*Sqrside;
+    printf("The perimeter of a Square:%.2f\n",Sqrperimeter); 
+    return 0;
+}
