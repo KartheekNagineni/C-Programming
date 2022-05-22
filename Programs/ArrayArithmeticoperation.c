5f4d1cc4ccdacf6a8f03205f6cef8303543c0b20
Kartheek Nagineni <79053787+naginenik@users.noreply.github.com>
2022-05-22 19:48:35 +0530

    Array Arithmetic Operation

---
+++ Programs/ArrayArithmeticoperation.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+ int Size,i,a[10],b[10];
+ int Addition[10],Subtraction[10],Multiplication[10],Module[10];
+ float Division[10];
+ printf("Enter the Size of the Array:");
+ scanf("%d",&Size);
+ printf("Enter the First Array Elements:");
+ for(i=0;i<Size;i++)
+  {
+      scanf("%d",&a[i]);
+  }
+ printf("Enter the Second Array Elements:");
+ for(i=0;i<Size;i ++)
+  {
+      scanf("%d",&b[i]);
+  }
+  for(i=0;i<Size;i ++)
+  {
+      Addition [i]=a[i]+b[i];
+      Subtraction [i]=a[i]-b[i];
+      Multiplication [i]=a[i]*b[i];
+      Division [i]=a[i]/b[i];
+      Module [i]=a[i]%b[i]; 
+  }
+ printf("\n Add\t Sub\t Multi\t Div\t Mod");
+ for(i=0;i<Size;i++)
+  {
+      printf("\n%d \t",Addition[i]);
+      printf("%d \t",Subtraction[i]);
+      printf("%d \t",Multiplication[i]);
+      printf("%.2f \t",Division[i]);
+      printf("%d \t",Module[i]);
+  }
+  return 0;
+} 
