553480d8190a48c123a106b9c20ff8b9c74182a3
Kartheek Nagineni <79053787+naginenik@users.noreply.github.com>
2022-05-25 18:18:59 +0530

    Convert uppercase to lowercase

---
+++ Programs/UppercasetoLowercase.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  	char Str1[100];
+  	int i;
+  	printf("Enter a String to Convert into Lowercase:");
+  	gets(Str1);
+  	for (i=0;Str1[i]!='\0';i++)
+  	{
+  		if(Str1[i]>='A'&&Str1[i]<='Z')
+  		{
+  			Str1[i]=Str1[i]+32;
+		}
+  	}
+  	printf("The given String in Lower Case:%s",Str1);
+  	return 0;
+}
