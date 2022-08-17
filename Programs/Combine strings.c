d1de78bd871583fdb015cd038dc9a6bffd4838cc
Kartheek Nagineni <79053787+naginenik@users.noreply.github.com>
2022-08-17 21:12:39 +0530

    To combine two strings

---
+++ Programs/Combine strings.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void concatenate(char[],char[]);
+int main()
+{
+   char p[100],q[100];
+   printf("Input a string:\n");
+   gets(p);
+   printf("Input a string to combine:\n");
+   gets(q);
+   concatenate(p,q);
+   printf("combined string is:\"%s\"\n",p);
+   return 0;
+}
+void concatenate(char p[],char q[]){
+   int c,d;
+   c=0;
+   while (p[c]!='\0'){
+      c++;      
+   }
+   d=0;
+   while (q[d]!='\0'){
+      p[c]=q[d];
+      d++;
+      c++;    
+   }
+   p[c]='\0';
+}
